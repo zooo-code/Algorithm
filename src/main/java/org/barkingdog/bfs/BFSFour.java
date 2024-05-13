1e8885106682e4a8a16b84ca57bcf5007a9b5dcb
zoo-code <kyj20908@naver.com>
2024-05-13 21:49:42 +0900

    알고리즘 공부 시작 1

---
+++ src/main/java/org/barkingdog/bfs/BFSFour.java
@@ -0,0 +1,16 @@
+package org.barkingdog.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BFSFour {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = br.readLine().split(" ");
+
+    }
+}
